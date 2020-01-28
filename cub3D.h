1007bb9e7ab23d3db0298179c6cdc9e5b0a7fc11
BooDeer <hsissi3a@gmail.com>
2020-01-28 18:23:10 +0100

    cub3D

--- cub3D.h
+++ cub3D.h
@@ -6,7 +6,7 @@
 /*   By: hboudhir <hboudhir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/25 09:55:00 by hboudhir          #+#    #+#             */
-/*   Updated: 2020/01/25 15:25:23 by hboudhir         ###   ########.fr       */
+/*   Updated: 2020/01/28 17:45:27 by hboudhir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,11 @@
 #include <stdlib.h>
 #include <mlx.h>
 
+typedef struct 
+{
+	int x;
+	int y;
+}	point;
 
 #define MAP_NUM_ROWS 11
 #define MAP_NUM_COLS 15
--- main.c
+++ main.c
@@ -6,15 +6,16 @@
 /*   By: hboudhir <hboudhir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/04 08:12:18 by hboudhir          #+#    #+#             */
-/*   Updated: 2020/01/25 15:42:16 by hboudhir         ###   ########.fr       */
+/*   Updated: 2020/01/28 18:09:36 by hboudhir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
+#include <stdio.h>
 
 int map[MAP_NUM_ROWS][MAP_NUM_COLS] = {
 			{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+            {1, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
             {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1},
             {1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1},
             {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1},
@@ -27,10 +28,17 @@ int map[MAP_NUM_ROWS][MAP_NUM_COLS] = {
 };
 void	*color_buffer_texture;
 int		*data;
-// void	*create_block()
-// {
-			
-// }
+int		playerX = 0, playerY = 0;
+void	*mlx_ptr;
+void	*win_ptr;
+void		put_pixel(int x, int y, int color, void *img)
+{
+	int k;
+	int *tmp;
+	
+	tmp = (int *)mlx_get_data_addr(img, &k, &k, &k);
+	tmp[x + (y * WINDOW_WIDTH)] = color;
+}
 
 void draw_square(int x, int y,int width,int color)
 {
@@ -40,51 +48,85 @@ void draw_square(int x, int y,int width,int color)
 		j = 0;
 		while(j < width)
 		{
-			data[((i + y) * WINDOW_WIDTH)	+(j + x)] = color;
+			// data[((i + y) * WINDOW_WIDTH) + (j + x)] = color;
+			put_pixel((j + x), (i + y), color, color_buffer_texture);
 			j++;
 		}
 		i++;
 	}		
 }
 
-int		main()
+void 	draw_map(point *pl)
 {
-
-	void	*mlx_ptr;
-	void	*win_ptr;
-	int		a;
-	
-	
-
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr,WINDOW_WIDTH, WINDOW_HEIGHT,"bruh1");
-	color_buffer_texture = mlx_new_image(mlx_ptr, WINDOW_WIDTH,WINDOW_HEIGHT);
-	data = (int*)mlx_get_data_addr(color_buffer_texture, &a,&a,&a);
-	//mlx_put_image_to_window(mlx_ptr,win_ptr,color_buffer_texture,0,0);
-		
-	for (int i = 0; i < MAP_NUM_COLS; i++)
+	int x, y;
+	static int check;
+		for (int i = 0; i < MAP_NUM_COLS; i++)
 	{
 		int j = 0;
 		for (int j = 0; j < MAP_NUM_ROWS; j++)
 		{
 			if(map[j][i] == 1)
 			{
-				//printf("%d;%d\n",i,j);
 				draw_square(i * TILE_SIZE,j * TILE_SIZE, TILE_SIZE, 0xff);
 			}
-			//else
-			//	draw_square(i * TILE_SIZE,j * TILE_SIZE, 10, 0xff00ff);
-			//printf("%d;%d\n",i,j);
-			// else
-				//draw_square(i * TILE_SIZE,j * TILE_SIZE, TILE_SIZE, 0);
-	// 		map[i][j] == 1 ? 
-	// 		mlx_pixel_put(mlx_ptr, mlx_win, i * WINDOW_WIDTH/MAP_NUM_ROWS, j * WINDOW_WIDTH/MAP_NUM_COLS, 255255255) 
-	// 		: mlx_pixel_put(mlx_ptr, mlx_win, i * WINDOW_WIDTH/MAP_NUM_ROWS, j * WINDOW_WIDTH/MAP_NUM_COLS, 255);
+			else if (!map[j][i])
+				draw_square(i * TILE_SIZE,j * TILE_SIZE, TILE_SIZE, 0xff00);
+			else if (!check++)
+			{
+				x = (i * TILE_SIZE) + TILE_SIZE / 2;
+				y = (j * TILE_SIZE) + TILE_SIZE / 2;
+				pl->x = x;
+				pl->y = y;
+				put_pixel(x, y, 0xffffff, color_buffer_texture);
+				printf("%d\n%d\n", x, y);
+			}
+			
 		}
 	}
-	//draw_square(100,100,100,0x99ffff);	
+}
+int 	move_player(int key, point *pl)
+{
+	if (key == 126)
+	{
+		pl->y -= TILE_SIZE / 4;
+	}
+	else if (key == 125)
+		pl->y += TILE_SIZE / 4;
+	else if (key == 124)
+		pl->x += TILE_SIZE / 4;
+	else if (key == 123)
+		pl->x -= TILE_SIZE / 4;
+	mlx_destroy_image(mlx_ptr, color_buffer_texture);
+	color_buffer_texture = mlx_new_image(mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
+	mlx_clear_window(mlx_ptr, win_ptr);
+	draw_map(pl);
+	put_pixel(pl->x, pl->y, 0xffffff, color_buffer_texture);
+	mlx_put_image_to_window(mlx_ptr, win_ptr, color_buffer_texture, 0, 0);
+	return 0;
+}
+
+int		move_p(point *pl)
+{
+	mlx_hook(win_ptr, 2, 0, move_player,pl);
+	printf("ppppp\n");
+	return 0;
+}
+
+int		main()
+{
+
+
+	int		a;
+	point pl;
+
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr,WINDOW_WIDTH, WINDOW_HEIGHT,"bruh");
+	color_buffer_texture = mlx_new_image(mlx_ptr, WINDOW_WIDTH,WINDOW_HEIGHT);
+	data = (int*)mlx_get_data_addr(color_buffer_texture, &a,&a,&a);
+	draw_map(&pl);
+	// mlx_hook(win_ptr, 2, 0, move_player,&pl);
+	mlx_loop_hook(mlx_ptr, move_p, &pl);
+	// mlx_loop_hook(mlx_ptr, playermove, &a);
 	mlx_put_image_to_window(mlx_ptr,win_ptr,color_buffer_texture,0,0);
-	// mlx_pixel_put(mlx_ptr, mlx_win, 0, 0, 0xFFFFFF);
-	// mlx_new_image(mlx_ptr, 100, 100);
 	mlx_loop(mlx_ptr);
 }
