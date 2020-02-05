649086204461a8ffb254cdbd31e6a397ae68f9e9
BooDeer <hsissi3a@gmail.com>
2020-02-05 18:13:13 +0100

    cub3D

--- cub3D.h
+++ cub3D.h
@@ -6,7 +6,7 @@
 /*   By: hboudhir <hboudhir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/25 09:55:00 by hboudhir          #+#    #+#             */
-/*   Updated: 2020/01/28 17:45:27 by hboudhir         ###   ########.fr       */
+/*   Updated: 2020/02/05 14:31:59 by hboudhir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,28 @@
 #include <string.h>
 #include <stdlib.h>
 #include <mlx.h>
+#include <math.h>
 
 typedef struct 
 {
-	int x;
-	int y;
+	float x;
+	float y;
+	float	radius;
+	int turnDirection;
+	int walkDirection;
+	float rotationAngle;
+	float moveSpeed;
+	float rotationSpeed;
+	float turnSpeed;
+	void	*mlx_ptr;
+	void	*win_ptr;
+	
 }	point;
 
-#define MAP_NUM_ROWS 11
+
+#define MINIMAP_SCALE 1.0
+#define MAP_NUM_ROWS 12
 #define MAP_NUM_COLS 15
 #define TILE_SIZE 64
-#define WINDOW_WIDTH TILE_SIZE * MAP_NUM_COLS
+#define WINDOW_WIDTH TILE_SIZE * 	
 #define WINDOW_HEIGHT TILE_SIZE * MAP_NUM_ROWS
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hboudhir <hboudhir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/04 08:12:18 by hboudhir          #+#    #+#             */
-/*   Updated: 2020/01/28 18:09:36 by hboudhir         ###   ########.fr       */
+/*   Updated: 2020/02/05 18:12:58 by hboudhir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,34 @@ int map[MAP_NUM_ROWS][MAP_NUM_COLS] = {
             {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
             {1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1},
             {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
             {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 };
 void	*color_buffer_texture;
 int		*data;
-int		playerX = 0, playerY = 0;
-void	*mlx_ptr;
-void	*win_ptr;
+
+float		abso(float n)
+{
+	return ((n > 0) ? n : (n * -1));
+}
+/*
+**
+**
+**
+**
+*/
+void	struct_init(point *pl)
+{
+	pl->x = 0;
+	pl->y = 0;
+	pl->radius = 3;
+	pl->turnDirection = 0;
+	pl->walkDirection = 0 ;
+	pl->rotationAngle = M_PI / 2;
+	pl->moveSpeed = 2.0;
+	pl->rotationSpeed = 2 * (M_PI  / 180);
+	pl->turnDirection = 45 * (M_PI / 180);
+}
 void		put_pixel(int x, int y, int color, void *img)
 {
 	int k;
@@ -56,6 +77,30 @@ void draw_square(int x, int y,int width,int color)
 	}		
 }
 
+void	draw_line(point *pl, float x1, float y1)
+{
+	float dx, dy, step;
+	int		i;
+
+	i = -1;
+	dx = abso(x1 - pl->x);
+	dy = abso(y1 - pl->y);
+
+	if (dx >= dy)
+		step = dx;
+	else
+		step = dy;
+	dx /= step;
+	dy /= step;
+	x1 = pl->x;
+	y1 = pl->y;
+	while (++i < step)
+	{
+		put_pixel(x1 * MINIMAP_SCALE, y1 * MINIMAP_SCALE, 0xffffff, color_buffer_texture);
+		x1 += dx;
+		y1 += dy;
+	}
+}
 void 	draw_map(point *pl)
 {
 	int x, y;
@@ -67,10 +112,10 @@ void 	draw_map(point *pl)
 		{
 			if(map[j][i] == 1)
 			{
-				draw_square(i * TILE_SIZE,j * TILE_SIZE, TILE_SIZE, 0xff);
+				draw_square(i * TILE_SIZE * MINIMAP_SCALE,j * TILE_SIZE * MINIMAP_SCALE, TILE_SIZE * MINIMAP_SCALE, 0xff);
 			}
 			else if (!map[j][i])
-				draw_square(i * TILE_SIZE,j * TILE_SIZE, TILE_SIZE, 0xff00);
+				draw_square(i * TILE_SIZE * MINIMAP_SCALE,j * TILE_SIZE * MINIMAP_SCALE, TILE_SIZE * MINIMAP_SCALE, 0x000000);
 			else if (!check++)
 			{
 				x = (i * TILE_SIZE) + TILE_SIZE / 2;
@@ -78,37 +123,72 @@ void 	draw_map(point *pl)
 				pl->x = x;
 				pl->y = y;
 				put_pixel(x, y, 0xffffff, color_buffer_texture);
+				draw_line(pl, 0, 0);
 				printf("%d\n%d\n", x, y);
 			}
 			
 		}
 	}
 }
+
 int 	move_player(int key, point *pl)
 {
-	if (key == 126)
+	float	moveStep, newPlayerx, newPlayery;
+
+
+	printf("%d\n", key);
+	if (key == 13)
 	{
-		pl->y -= TILE_SIZE / 4;
+		pl->y -= 3;
+		pl->walkDirection = 1;
 	}
-	else if (key == 125)
-		pl->y += TILE_SIZE / 4;
-	else if (key == 124)
-		pl->x += TILE_SIZE / 4;
-	else if (key == 123)
-		pl->x -= TILE_SIZE / 4;
-	mlx_destroy_image(mlx_ptr, color_buffer_texture);
-	color_buffer_texture = mlx_new_image(mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
-	mlx_clear_window(mlx_ptr, win_ptr);
+	else if (key == 1)
+	{
+		pl->y += 3;
+		pl->walkDirection = -1;
+	}
+	else if (key == 2)
+	{
+		pl->x += 3;
+		pl->turnDirection = 1;
+	}
+	else if (key == 0)
+	{
+		pl->x -= 3;
+		pl->turnDirection = -1;
+	}
+	else if (key == 53)
+		exit(1);
+	mlx_destroy_image(pl->mlx_ptr, color_buffer_texture);
+	color_buffer_texture = mlx_new_image(pl->mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
+	// mlx_clear_window(pl->mlx_ptr, pl->win_ptr);
 	draw_map(pl);
+
+
+	
+	pl->rotationAngle += pl->turnDirection * pl->turnSpeed;
+	moveStep = pl->walkDirection * pl->moveSpeed;
+	newPlayerx = pl->x + cos(pl->rotationAngle) * moveStep;
+	newPlayery = pl->y + sin(pl->rotationAngle) * moveStep;
+
+
+
+	
 	put_pixel(pl->x, pl->y, 0xffffff, color_buffer_texture);
-	mlx_put_image_to_window(mlx_ptr, win_ptr, color_buffer_texture, 0, 0);
+	draw_line(pl, newPlayerx, newPlayery);
+	mlx_put_image_to_window(pl->mlx_ptr, pl->win_ptr, color_buffer_texture, 0, 0);
 	return 0;
+}
+
+void	reset_player(int key, point *pl)
+{
+	struct_init(pl);
 }
 
 int		move_p(point *pl)
 {
-	mlx_hook(win_ptr, 2, 0, move_player,pl);
-	printf("ppppp\n");
+	mlx_hook(pl->win_ptr, 2, 0, move_player,pl);
+	mlx_hook(pl->mlx_ptr, 3, 0, reset_player, pl);
 	return 0;
 }
 
@@ -118,15 +198,21 @@ int		main()
 
 	int		a;
 	point pl;
+	void *img;
 
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr,WINDOW_WIDTH, WINDOW_HEIGHT,"bruh");
-	color_buffer_texture = mlx_new_image(mlx_ptr, WINDOW_WIDTH,WINDOW_HEIGHT);
+	pl.mlx_ptr = mlx_init();
+	pl.win_ptr = mlx_new_window(pl.mlx_ptr,WINDOW_WIDTH, WINDOW_HEIGHT,"bruh");
+	color_buffer_texture = mlx_new_image(pl.mlx_ptr, WINDOW_WIDTH,WINDOW_HEIGHT);
 	data = (int*)mlx_get_data_addr(color_buffer_texture, &a,&a,&a);
 	draw_map(&pl);
-	// mlx_hook(win_ptr, 2, 0, move_player,&pl);
-	mlx_loop_hook(mlx_ptr, move_p, &pl);
-	// mlx_loop_hook(mlx_ptr, playermove, &a);
-	mlx_put_image_to_window(mlx_ptr,win_ptr,color_buffer_texture,0,0);
-	mlx_loop(mlx_ptr);
+	// mlx_hook(pl->win_ptr, 2, 0, move_player,&pl);
+	mlx_loop_hook(pl.mlx_ptr, move_p, &pl);
+
+
+	// img = mlx_xpm_file_to_image(pl->mlx_ptr, "~/Downloads/81621207_667657023772881_3948275671851073536_n.png", &a, &a);
+
+	
+	// mlx_loop_hook(pl->mlx_ptr, playermove, &a);
+	mlx_put_image_to_window(pl.mlx_ptr,pl.win_ptr,color_buffer_texture,0,0);
+	mlx_loop(pl.mlx_ptr);
 }
